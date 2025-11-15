8b3f5a95d0cd87ee3460fb38db917bc726c0afe8
Marveltechapps <marveltech32@gmail.com>
2025-11-15 18:05:11 +0530

    Express Route Updated

--- src/app.js
+++ src/app.js
@@ -24,8 +24,9 @@ const notificationRoutes = require('./routes/notificationRoutes');
 const app = express();
 app.use(cors());
 app.use(morgan('dev'));
-// Middleware to parse JSON requests
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
 // MongoDB connection setup
 mongoose.connect(process.env.MONGODB_URI, {
